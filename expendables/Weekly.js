58bdded54f5da9e3ede09a2b6e39c6e71f55d888
Dj12amz <jose_mvp55@hotmail.com>
2018-06-11 17:41:51 -0500

    Fixed errors in weekñly and monthly modals

--- expendables/Weekly.js
+++ expendables/Weekly.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput, Button, TouchableHighlight, Alert } from 'react-native';
+import TransactionModal from './Modal';
 export default class Weekly extends React.Component {
     constructor(props) {
         super(props)
