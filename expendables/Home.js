2bece727a5a38d630455944a4aa1805a356b2ecb
Dj12amz <jose_mvp55@hotmail.com>
2018-06-10 20:43:31 -0500

    Full Navigation and login

--- expendables/Home.js
+++ expendables/Home.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, TouchableHighlight, Alert } from 'react-native';
-export default class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state =
-            {
+import * as React from 'react';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
+import Daily from './Daily';
+import Weekly from './Weekly';
+import Monthly from './Monthly';
 
-            }
-    }
+const ShowDaily = () => (
+    <Daily />
+);
+const ShowWeekly = () => (
+    <Weekly />
+);
+const ShowMonthly = () => (
+    <Monthly />
+);
+
+export default class Home extends React.Component {
+    state = {
+        index: 0,
+        routes: [
+            { key: 'first', title: 'Daily' },
+            { key: 'second', title: 'Weekly' },
+            { key: 'third', title: 'Monthly' },
+
+        ],
+    };
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Hola: {this.props.nick}</Text>
-            </View>
-        )
+            <TabView
+                navigationState={this.state}
+                renderScene={SceneMap({
+                    first: ShowDaily,
+                    second: ShowWeekly,
+                    third: ShowMonthly
+                })}
+                onIndexChange={index => this.setState({ index })}
+                initialLayout={{ width: Dimensions.get('window').width }}
+            />
+        );
     }
 }
---
+++ expendables/Weekly.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableHighlight, Alert } from 'react-native';
+export default class Weekly extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state =
+            {
+
+            }
+    }
+    render() {
+        return (
+            <View>
+                <Text>Weekly</Text>
+            </View>
+        )
+    }
+
+
+}
