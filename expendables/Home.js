4c1c90f7be52ac633290500a3fa31b1161b68356
Sebastian Arredondo <jsebastanav8@gmail.com>
2018-06-11 21:40:44 -0500

    UI updates, Action Button.

--- expendables/Home.js
+++ expendables/Home.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { View, StyleSheet, Dimensions, Text } from 'react-native';
 import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 import Tabbar from 'react-native-tabbar-bottom';
+import ActionButton from 'react-native-action-button';
 import Transactions from './Transactions';
 export default class Home extends React.Component {
     constructor(props) {
@@ -21,7 +22,10 @@ export default class Home extends React.Component {
                 {this.state.page === "Transactions" && <Transactions />}
                 {this.state.page === "Stats" && <Text>Coming soon</Text>}
                 {this.state.page === "Settings" && <Text>Coming soon</Text>}
-
+                <ActionButton
+  buttonColor="rgba(231,76,60,1)"
+  onPress={() => { console.log("hi")}}
+/>
                 <Tabbar
                     stateFunc={(tab) => {
                         this.setState({ page: tab.page })
