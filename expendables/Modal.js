007e362c0cba262da2a5ec53209e7b4850b0eae7
Unknown <sebastian@cbqasolutions.com>
2018-06-11 19:36:54 -0500

    Modal base finished

--- expendables/Modal.js
+++ expendables/Modal.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
-import { Modal, Text, TouchableHighlight, View, TextInput, Button } from 'react-native';
-import { CheckBox } from 'react-native-elements'
+import { StyleSheet, Modal, Text, TouchableHighlight, View, TextInput, Button, ListView } from 'react-native';
+import { CheckBox } from 'react-native-elements';
+import DatePicker from 'react-native-datepicker';
+import ActionSheet from 'react-native-actionsheet';
 
 export default class TransactionModal extends React.Component {
     constructor(props) {
         super(props)
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         this.state = {
             modalVisible: true,
-            checked: false,
-            checked2: false
+            checked: true,
+            checked2: false,
+            date: "2016-05-15",
+            txtAmount: "0.0",
+            dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+            category : "Choose Category",
+            categories : ["General","Food","School","Transportation" ]
         };
         this.setModalVisible = this.setModalVisible.bind(this);
-        
+        this.incomeChecked = this.incomeChecked.bind(this);
+        this.expenseChecked = this.expenseChecked.bind(this);
     }
+    showActionSheet = () => {
+        this.ActionSheet.show()
+      }
 
 
     setModalVisible() {
         this.props.setModalInvisible();
+    }
+    incomeChecked() {
+        this.setState({ checked: true, checked2: false });
+    }
+    expenseChecked() {
+        this.setState({ checked: false, checked2: true });
     }
     render() {
         return (
@@ -28,19 +46,53 @@ export default class TransactionModal extends React.Component {
                     alert('Modal has been closed.');
                 }}>
                 <View style={{ marginTop: 200 }}>
-                    <View>
-                        <Text>Add Transaction </Text>
-                        <TextInput placeholder="Enter Date" />
-                        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                        <CheckBox 
-                            title='Click Here'
-                            checked={this.state.checked}
+                    <View style={{
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                        <Text style={{fontSize:28}}>Add Transaction </Text>
+                        <DatePicker
+                            style={{ width: 200 }}
+                            date={this.state.date}
+                            mode="date"
+                            placeholder="select date"
+                            format="YYYY-MM-DD"
+                            minDate="2016-05-01"
+                            maxDate="2016-06-01"
+                            confirmBtnText="Confirm"
+                            cancelBtnText="Cancel"
+                            customStyles={{
+                                dateIcon: {
+                                    position: 'absolute',
+                                    left: 0,
+                                    top: 4,
+                                    marginLeft: 0
+                                },
+                                dateInput: {
+                                    marginLeft: 36
+                                }
+                            }}
+                            onDateChange={(date) => { this.setState({ date: date }) }}
                         />
-                        <CheckBox
-                            title='Click Here'
-                            checked={this.state.checked2}
-                        />
+                        <TextInput style={{ width: '100%' }} placeholder="Amount" onChangeText={(txtAmount) => this.setState({ txtAmount })} />
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <CheckBox onPress={this.incomeChecked}
+                                title='Income'
+                                checked={this.state.checked}
+                            />
+                            <CheckBox onPress={this.expenseChecked}
+                                title='Expense'
+                                checked={this.state.checked2}
+                            />
                         </View>
+                        <Text style={{fontSize : 28}} onPress={this.showActionSheet}>{this.state.category}</Text>
+                        <ActionSheet
+                            ref={o => this.ActionSheet = o}
+                            title={'Which one do you like ?'}
+                            options={this.state.categories}
+                            onPress={(index) => { this.setState({category: this.state.categories[index]}) }}
+                        />
                         <Button title="Ok" onPress={this.setModalVisible} />
                         <Button title="Cancel" onPress={this.setModalVisible} />
                     </View>
@@ -49,3 +101,14 @@ export default class TransactionModal extends React.Component {
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
